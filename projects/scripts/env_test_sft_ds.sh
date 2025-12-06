a6f916f881796b346fd46c507973e587695cb63f
zsxm1998 <zsxm1998@qq.com>
2025-12-06 18:59:22 +0800

    test for Geometry3K on SFT and GRPO

--- projects/scripts/env_test_sft_ds.sh
+++ projects/scripts/env_test_sft_ds.sh
@@ -8,14 +8,15 @@ swift sft \
     --torch_dtype bfloat16 \
     --gradient_checkpointing true \
     --attn_impl flash_attn \
-    --dataset 'AI-ModelScope/alpaca-gpt4-data-zh#300' \
+    --dataset 'hf::hiyouga/geometry3k:train' \
+    --val_dataset 'hf::hiyouga/geometry3k:validation' \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --learning_rate 1e-4 \
     --gradient_accumulation_steps 16 \
-    --eval_steps 50 \
-    --save_steps 50 \
+    --eval_steps 10 \
+    --save_steps 10 \
     --save_total_limit 2 \
     --logging_steps 1 \
     --max_length 2048 \

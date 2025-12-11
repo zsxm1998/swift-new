f70991f2b47f51400de49edbda56759b5415e5fe
zsxm1998 <zsxm1998@qq.com>
2025-12-12 01:42:18 +0800

    optimize the env install and test scripts

--- projects/scripts/env_test_sft_ds.sh
+++ projects/scripts/env_test_sft_ds.sh
@@ -1,15 +1,17 @@
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
 NPROC_PER_NODE=8 \
 swift sft \
-    --model Qwen/Qwen3-VL-8B-Instruct \
+    --model Qwen/Qwen2.5-VL-7B-Instruct \
     --output_dir ./outputs/env_test \
     --train_type full \
     --deepspeed zero2 \
     --torch_dtype bfloat16 \
     --gradient_checkpointing true \
     --attn_impl flash_attn \
-    --dataset 'hf::hiyouga/geometry3k:train' \
-    --val_dataset 'hf::hiyouga/geometry3k:validation' \
+    --dataset 'hf::PAPOGalaxy/PAPO_ViRL39K_train' \
+    --val_dataset 'hf::PAPOGalaxy/PAPO_MMK12_test' \
+    --dataset_num_proc 32 \
+    --load_from_cache_file true \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
